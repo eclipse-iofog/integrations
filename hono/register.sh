04a98531cbba362284b3da1b6ebe355714a16a82
SergeRadinovich <serge@edgeworx.io>
2020-01-28 14:38:19 +1300

    Change output of register.sh

--- hono/register.sh
+++ hono/register.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-set -x
-set -e
-
 REGISTRY_IP=$(kubectl get svc | grep hono-service-device-registry-ext | awk '{print $4}' | tr -d '"')
 MY_TENANT=$(curl -si -X POST http://$REGISTRY_IP:28080/v1/tenants | tail -n1 | jq .id | tr -d '"')
 MY_DEVICE=$(curl -si -X POST http://$REGISTRY_IP:28080/v1/devices/$MY_TENANT | tail -n1 | jq .id | tr -d '"')
@@ -14,3 +11,8 @@ curl -i -X PUT -H "content-type: application/json" --data-binary '[{
       "pwd-plain": "'$MY_PWD'"
   }]
 }]' http://$REGISTRY_IP:28080/v1/credentials/$MY_TENANT/$MY_DEVICE
+
+echo "REGISTRY_IP=$REGISTRY_IP
+MY_TENANT=$MY_TENANT
+MY_DEVICE=$MY_DEVICE
+MY_PWD=$MY_PWD"
