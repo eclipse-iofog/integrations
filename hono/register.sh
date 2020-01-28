99a0e0ec07e7ea8b8774b43218d7105d65581e91
SergeRadinovich <serge@edgeworx.io>
2020-01-28 14:36:05 +1300

    Fix password in register.sh

--- hono/register.sh
+++ hono/register.sh
@@ -6,7 +6,7 @@ set -e
 REGISTRY_IP=$(kubectl get svc | grep hono-service-device-registry-ext | awk '{print $4}' | tr -d '"')
 MY_TENANT=$(curl -si -X POST http://$REGISTRY_IP:28080/v1/tenants | tail -n1 | jq .id | tr -d '"')
 MY_DEVICE=$(curl -si -X POST http://$REGISTRY_IP:28080/v1/devices/$MY_TENANT | tail -n1 | jq .id | tr -d '"')
-MY_PASS=my-pwd
+MY_PWD=my-pwd
 curl -i -X PUT -H "content-type: application/json" --data-binary '[{
   "type": "hashed-password",
   "auth-id": "'$MY_DEVICE'",
